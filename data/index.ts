4923f9c1972b7d28e78308d1536d2c8edb76bd41
samuelscorporation86@gmail.com <samuelscorporation86@gmail.com>
2024-07-26 12:43:03 -0500

    Initial commit

---
+++ data/index.ts
@@ -0,0 +1,208 @@
+export const navItems = [
+  { name: "Acerca de mi", link: "#about" },
+  { name: "Projectos", link: "#projects" },
+  { name: "Testimonios", link: "#testimonials" },
+  { name: "Contactame :D", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "Priorizo ​​la colaboración con el cliente, fomentando la comunicación abierta.",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Soy muy flexible con las comunicaciones de zona horaria.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "Mejoras en programas",
+    description: "Mejoro constantemente",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Entusiasta de la tecnología y apasionado por el desarrollo.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Actualmente construyendo una biblioteca de animación JS",
+    description: "La primicia interna",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Los mejores proyectos que te puedes imaginar!!",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Planetas del sistema solar 3D para explorar",
+    des: "Explora las maravillas de nuestro sistema solar con esta cautivadora simulación 3D de los planetas usando Three.js.",
+    img: "/p1.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "/ui.earth.com",
+  },
+  {
+    id: 2,
+    title: "CRM - Aplicaciones para clientes",
+    des: "Gestion de cliente y equipo para el desarrollo correcto de la empresa",
+    img: "/p2.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "/ui.yoom.com",
+  },
+  {
+    id: 3,
+    title: "Aplicaciones de videojuegos!! para fomentar tu creatividad",
+    des: "Aplicacion de videojuegos, RUFF, vive una hermosa experiencia en este fascinante juego",
+    img: "/p3.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "/ui.aiimg.com",
+  },
+  {
+    id: 4,
+    title: "Diseños web Atractivas para todo público",
+    des: "Diseños y responsive para todo tipo de usuarios que desean conocer cada uno de mis proyectos.",
+    img: "/p4.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "/ui.apple.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Colaborar con Samuel fue un absoluto placer. Su profesionalismo, rapidez y dedicación para lograr resultados excepcionales fueron evidentes a lo largo de nuestro proyecto. Realmente destaca el entusiasmo de Adrian por cada faceta del desarrollo. Si busca mejorar su sitio web y su marca, Samuel es el socio ideal.",
+    name: "Diego Mora",
+    title: "Desarrollador de aplicaciones Móviles",
+  },
+  {
+    quote:
+      "Trabajar con Samuel ha sido una experiencia sumamente gratificante. Su compromiso, eficiencia y atención al detalle se reflejaron en cada etapa de nuestro proyecto. Además, la pasión de Laura por cada aspecto del desarrollo es realmente inspiradora. Si buscas llevar tu sitio web y marca al siguiente nivel, Samuel es la elección perfecta.",
+    name: "Laura Rodriguéz",
+    title: "Líder de desarrollo Front-End",
+  },
+  {
+    quote:
+      "Colaborar con esta persona me ayudo a mejorar en diferentes ambitos profesionales relacionados a la programación y al desarrollo front-end",
+    name: "Jose Luis",
+    title: "Desarrollador Front-end",
+  }
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Practicante de Ingeniería Frontend",
+    desc: "Asistí en el desarrollo de una plataforma basada en web utilizando React.js, mejorando la interactividad.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Desarrollo de aplicaciones móviles - JSM Tech",
+    desc: "Diseñé y desarrollé una aplicación móvil para plataformas iOS y Android utilizando React Native.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Proyecto de desarrollo de aplicaciones independientes",
+    desc: "Lideré el desarrollo de una aplicación móvil para un cliente, desde el concepto inicial hasta la implementación en las tiendas de aplicaciones.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Desarrollador Frontend Capacitado",
+    desc: "Desarrollé y mantuve funciones orientadas al usuario utilizando tecnologías frontend modernas.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/Dayshade71"
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    link: "https://twitter.com/tu-perfil"
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/samuel-david-molina-prieto-a9b666241/?trk=opento_sprofile_details"
+  },
+];
